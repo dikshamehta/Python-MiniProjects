7e4dce51f4886760f420f708d7dbb925ca30a3db
dikshamehta <500061597@stu.upes.ac.in>
2019-06-30 23:28:03 +0530

    second commit

--- DataStructures/LinkedList.py
+++ DataStructures/LinkedList.py
@@ -1,8 +1,3 @@
-class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next= None
-
 '''n1= Node(10)
 n2= Node(20)
 
@@ -13,8 +8,44 @@ print(head.data)
 aise krna coding wise galat nai hai but linked list mai we can not make reference to each node, hume sbko head
 ke through hi access krna hota hai. so better is this...
 '''
-head = Node(2)
-head.next = Node(1)
 
-print(head.data)
-print(head.next.data)
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next= None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def generate_LinkedList(self,input_list):
+        for i in input_list:
+            if self.head is None:
+                self.head = Node(i)
+                self.tail = self.head
+            else:
+                self.tail.next = Node(i) #connecting
+                self.tail = self.tail.next #moving tail to the last element of the linked list
+
+
+    def print_LinkedList(self):
+        temp=self.head
+        while temp is not None:
+            print((temp.data), end="")
+            temp= temp.next
+        print()
+    def appendToList(self,data):
+        if self.head is None:
+            self.head = Node(data)
+            self.tail = Node(data)
+        else:
+            self.tail.next=Node(data)
+            self.tail = self.tail.next
+
+
+l = LinkedList()
+input_list=[1,2,3,4,5]
+l.generate_LinkedList(input_list)
+l.print_LinkedList()
+l.appendToList(6)
+l.print_LinkedList()
